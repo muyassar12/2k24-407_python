11be1f71e842816ceae37072e1ff5750438e6b75
Javohir <azimovjavohir3442@gmail.com>
2025-05-16 10:48:38 +0500

    Task has been done

---
+++ Javohir/database.py
@@ -0,0 +1,36 @@
+# db.py
+
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_connection():
+    return psycopg2.connect(
+        dbname="selenium",
+        user="postgres",
+        password="password",
+        host="localhost",
+        port="5432"
+    )
+
+
+def create_table_if_not_exists():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Projects (
+            id SERIAL PRIMARY KEY,
+            header VARCHAR NOT NULL,
+            date VARCHAR,
+            image TEXT,
+            description TEXT
+        );
+    """)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
